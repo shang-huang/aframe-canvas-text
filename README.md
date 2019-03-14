1e49f0e8ccec4fef2fd22ae258265253dfbf6102
shang-huang <shang.cih.huang@gmail.com>
2019-03-14 16:18:53 +0800

    add "align" property ,code cleanup.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A component renders text from generated canvas for [A-Frame](https://aframe.io).
 ### Properties
 
 ```html
-<a-entity canvas-text="size:48; color:#0080FF; value:你好, HTML!\n你好, Aframe!"></a-entity>
+<a-entity canvas-text="size:48; color:#0080FF; align:center; value:你好, HTML!\n\u270C\n你好, Aframe!"></a-entity>
 ```
 
 | Property | Description                                               | Default Value |
@@ -13,3 +13,4 @@ A component renders text from generated canvas for [A-Frame](https://aframe.io).
 | size     | Text size, 100 is about 1 meter in space.                 | 50            |
 | color    | Text color.                                               | black         |
 | value    | Text to be rendered, supports unicode text & newline(\n). | ''            |
+| align    | Multi-line text alignment (left, center, right).          | 'left'        |
--- components/canvas-text.js
+++ components/canvas-text.js
@@ -6,11 +6,12 @@ AFRAME.registerComponent("canvas-text", {
   schema: {
     value: { type: "string" },
     size: { type: "int", default: 50 },
-    color: { type: "color", default: "black" }
+    color: { type: "color", default: "black" },
+    align: { type: "string", default: "left", oneOf: ["left", "center", "right"] }
   },
 
   init: function () {
-    // create canvas for texture, use parent uuid as id
+    // create canvas for text texture, use object3D uuid as id
     this.canvas = document.createElement("canvas");
     this.canvas.id = this.el.object3D.uuid;
     this.canvas.style.display = "none";
@@ -21,6 +22,9 @@ AFRAME.registerComponent("canvas-text", {
 
   update: function (oldData) {
     if (oldData != this.data) {
+      let _el = this.el;
+      let _canvas = this.canvas;
+      let _context = this.context;
       // get default font type & apply pixel ratio
       let _dpi = window.devicePixelRatio;
       let _fontType = window.getComputedStyle(document.body, null).getPropertyValue("font-family")
@@ -28,11 +32,12 @@ AFRAME.registerComponent("canvas-text", {
       // support unicode character
       let _uni_sentences = this.data.value.split("\\u");
       let _uni_sentence = "";
-      for (var i = 1; i < _uni_sentences.length; i++) {
-        let _unicode = _uni_sentences[i].substring(0, 4);
-        let _sentence = String.fromCharCode(parseInt(_unicode, 16)) + _uni_sentences[i].substring(4, _uni_sentences[i].length);
-        _uni_sentence += _sentence;
-      }
+      _uni_sentences.forEach((_sentence, index) => {
+        if (index > 0) {
+          let _unicode = _sentence.substring(0, 4);
+          _uni_sentence += String.fromCharCode(parseInt(_unicode, 16)) + _sentence.substring(4, _uni_sentences[index].length);
+        }
+      });
       let _newline_sentence = _uni_sentences[0] + _uni_sentence;
       let _sentences = _newline_sentence.replace(/\\n/g, "\n").split("\n");
       // get text area size & height of each lines
@@ -56,26 +61,42 @@ AFRAME.registerComponent("canvas-text", {
       });
       document.body.removeChild(_tempTextAreaEl);
       // set canvas size
-      this.canvas.width = _width;
-      this.canvas.height = _height;
+      _canvas.width = _width;
+      _canvas.height = _height;
       // clear previous text
-      this.context.clearRect(0, 0, _width, _height);
+      _context.clearRect(0, 0, _width, _height);
       // fill text to canvas
-      this.context.font = _font;
-      this.context.textBaseline = "middle";
-      this.context.fillStyle = this.data.color;
-      let _lineCount = 0;
+      _context.font = _font;
+      _context.textBaseline = "middle";
+      _context.fillStyle = this.data.color;
       let _fillHeight = 0;
-      _sentences.forEach(sentence => {
-        this.context.fillText(sentence, 0, (_lineHeights[_lineCount] / 2) + _fillHeight);
-        _fillHeight += _lineHeights[_lineCount];
-        _lineCount++;
+      _sentences.forEach((sentence, index) => {
+        let _y = (_lineHeights[index] / 2) + _fillHeight;
+        switch (this.data.align) {
+          case "left":
+            _context.textAlign = "left";
+            _context.fillText(sentence, 0, _y);
+            break;
+          case "center":
+            _context.textAlign = "center";
+            _context.fillText(sentence, _width / 2, _y);
+            break;
+          case "right":
+            _context.textAlign = "right";
+            _context.fillText(sentence, _width, _y);
+            break;
+          default:
+            _context.textAlign = "left";
+            _context.fillText(sentence, 0, _y);
+            break;
+        }
+        _fillHeight += _lineHeights[index];
       });
       // draw text
-      let objRatio = 0.0075 / _dpi;
-      this.el.setAttribute("geometry", { primitive: "plane", width: _width * objRatio, height: _height * objRatio });
-      this.el.removeAttribute("material");
-      this.el.setAttribute("material", { src: "#" + this.canvas.id.toString(), shader: "flat", npot: true, transparent: true });
+      let _objRatio = 0.0075 / _dpi;
+      _el.setAttribute("geometry", { primitive: "plane", width: _width * _objRatio, height: _height * _objRatio });
+      _el.removeAttribute("material");
+      _el.setAttribute("material", { src: "#" + _canvas.id.toString(), shader: "flat", npot: true, transparent: true });
     }
   },
 
