71f8c1210168827a1b94692b16a83b8f99d2453d
shang-huang <shang.cih.huang@gmail.com>
2019-05-22 13:26:47 +0800

    code refactoring, update packages

--- components/canvas-text.js
+++ components/canvas-text.js
@@ -10,7 +10,7 @@ AFRAME.registerComponent("canvas-text", {
     align: { type: "string", default: "left", oneOf: ["left", "center", "right"] }
   },
 
-  init: function () {
+  init() {
     // create canvas for text texture, use object3D uuid as id
     this.canvas = document.createElement("canvas");
     this.canvas.id = this.el.object3D.uuid;
@@ -21,100 +21,106 @@ AFRAME.registerComponent("canvas-text", {
   },
 
   update: function (oldData) {
-    if (oldData != this.data) {
-      let _el = this.el;
-      let _canvas = this.canvas;
-      let _context = this.context;
-      // get default font type & apply pixel ratio
-      let _dpi = window.devicePixelRatio;
-      let _fontType = window.getComputedStyle(document.body, null).getPropertyValue("font-family")
-      let _font = this.data.size * _dpi + "px " + _fontType;
-      // support unicode character
-      let _uni_sentences = this.data.value.split("\\u");
-      let _uni_sentence = "";
-      _uni_sentences.forEach((_sentence, index) => {
-        if (index === 0) {
-          _uni_sentence += _sentence;
-        } else {
-          let _unicodePlan1Plan2 = this.getUnicodeCharFromSentence(_sentence, 5);
-          if (/[\u{10000}-\u{2ffff}]/u.test(_unicodePlan1Plan2)) {
-            _uni_sentence += _unicodePlan1Plan2 + _sentence.substring(5, _uni_sentences[index].length);
-          } else {
-            let _unicodePlan0 = this.getUnicodeCharFromSentence(_sentence, 4);
-            if (/[\u{0000}-\u{ffff}]/u.test(_unicodePlan0)) {
-              _uni_sentence += _unicodePlan0 + _sentence.substring(4, _uni_sentences[index].length);
-            } else {
-              _uni_sentence += _sentence;
-            }
-          }
-        }
-      });
-      let _sentences = _uni_sentence.replace(/\\n/g, "\n").split("\n");
-      // get text area size & height of each lines
-      let _tempTextAreaEl = document.createElement("div");
-      _tempTextAreaEl.style.display = "inline-block";
-      _sentences.forEach(sentence => {
-        let __tempTextEl = document.createElement("span");
-        __tempTextEl.style.font = _font;
-        __tempTextEl.innerText = sentence;
-        _tempTextAreaEl.appendChild(__tempTextEl);
-        let __tempBreakEl = document.createElement("br");
-        __tempBreakEl.style.lineHeight = 0;
-        _tempTextAreaEl.appendChild(__tempBreakEl);
-      });
-      document.body.appendChild(_tempTextAreaEl);
-      let _width = _tempTextAreaEl.offsetWidth;
-      let _height = _tempTextAreaEl.offsetHeight;
-      let _lineHeights = [];
-      _tempTextAreaEl.childNodes.forEach(child => {
-        if (child.nodeName.toLowerCase() === "span") _lineHeights.push(child.offsetHeight);
-      });
-      document.body.removeChild(_tempTextAreaEl);
-      // set canvas size
-      _canvas.width = _width;
-      _canvas.height = _height;
-      // clear previous text
-      _context.clearRect(0, 0, _width, _height);
-      // fill text to canvas
-      _context.font = _font;
-      _context.textBaseline = "middle";
-      _context.fillStyle = this.data.color;
-      let _fillHeight = 0;
-      _sentences.forEach((sentence, index) => {
-        let _y = (_lineHeights[index] / 2) + _fillHeight;
-        switch (this.data.align) {
-          case "left":
-            _context.textAlign = "left";
-            _context.fillText(sentence, 0, _y);
-            break;
-          case "center":
-            _context.textAlign = "center";
-            _context.fillText(sentence, _width / 2, _y);
-            break;
-          case "right":
-            _context.textAlign = "right";
-            _context.fillText(sentence, _width, _y);
-            break;
-          default:
-            _context.textAlign = "left";
-            _context.fillText(sentence, 0, _y);
-            break;
-        }
-        _fillHeight += _lineHeights[index];
-      });
-      // draw text
-      let _objRatio = 0.0075 / _dpi;
-      _el.setAttribute("geometry", { primitive: "plane", width: _width * _objRatio, height: _height * _objRatio });
-      _el.removeAttribute("material");
-      _el.setAttribute("material", { src: "#" + _canvas.id.toString(), shader: "flat", npot: true, transparent: true });
-    }
+    (oldData != this.data) && this.draw();
   },
 
-  remove: function () {
+  draw() {
+    let _data = this.data;
+    let _el = this.el;
+    let _canvas = this.canvas;
+    let _context = this.context;
+    // get default font type & apply pixel ratio
+    let _dpi = window.devicePixelRatio;
+    let _fontType = window.getComputedStyle(document.body, null).getPropertyValue("font-family");
+    let _font = `${_data.size * _dpi}px ${_fontType}`;
+    // support unicode character & line breaks 
+    let _sentences = unicodeSentnece(_data.value).replace(/\\n/g, "\n").split("\n");
+    // get text area size & height of each lines
+    let _tempTextAreaEl = document.createElement("div");
+    _tempTextAreaEl.style.display = "inline-block";
+    _sentences.forEach(sentence => {
+      let __tempTextEl = document.createElement("span");
+      __tempTextEl.style.font = _font;
+      __tempTextEl.innerText = sentence;
+      _tempTextAreaEl.appendChild(__tempTextEl);
+      let __tempBreakEl = document.createElement("br");
+      __tempBreakEl.style.lineHeight = 0;
+      _tempTextAreaEl.appendChild(__tempBreakEl);
+    });
+    _el.appendChild(_tempTextAreaEl);
+    let _width = _tempTextAreaEl.offsetWidth;
+    let _height = _tempTextAreaEl.offsetHeight;
+    let _lineHeights = [];
+    _tempTextAreaEl.childNodes.forEach(child => (child.nodeName.toLowerCase() === "span") && _lineHeights.push(child.offsetHeight));
+    _el.removeChild(_tempTextAreaEl);
+    // set canvas size
+    _canvas.width = _width;
+    _canvas.height = _height;
+    // clear previous text
+    _context.clearRect(0, 0, _width, _height);
+    // fill text to canvas
+    _context.font = _font;
+    _context.textBaseline = "middle";
+    _context.fillStyle = _data.color;
+    let _fillHeight = 0;
+    _sentences.forEach((sentence, index) => {
+      let _y = (_lineHeights[index] / 2) + _fillHeight;
+      switch (_data.align) {
+        case "left":
+          _context.textAlign = "left";
+          _context.fillText(sentence, 0, _y);
+          break;
+        case "center":
+          _context.textAlign = "center";
+          _context.fillText(sentence, _width / 2, _y);
+          break;
+        case "right":
+          _context.textAlign = "right";
+          _context.fillText(sentence, _width, _y);
+          break;
+        default:
+          _context.textAlign = "left";
+          _context.fillText(sentence, 0, _y);
+          break;
+      }
+      _fillHeight += _lineHeights[index];
+    });
+    // draw text
+    let _objRatio = 0.0075 / _dpi;
+    _el.hasAttribute("geometry") && _el.removeAttribute("geometry");
+    _el.setAttribute("geometry", { primitive: "plane", width: _width * _objRatio, height: _height * _objRatio });
+    _el.hasAttribute("material") && _el.removeAttribute("material");
+    _el.setAttribute("material", { src: `#${_canvas.id}`, shader: "flat", npot: true, transparent: true });
+  },
+
+  remove() {
     this.el.innerHTML = "";
   },
-
-  getUnicodeCharFromSentence: function (sentence, digits) {
-    return String.fromCodePoint(parseInt(sentence.substring(0, digits), 16));
-  }
 });
+
+function unicodeSentnece(value) {
+  let _uni_sentences = value.split("\\u");
+  let _uni_sentence = "";
+  _uni_sentences.forEach((_sentence, index) => {
+    if (index === 0) {
+      _uni_sentence += _sentence;
+    } else {
+      let _unicodePlan1Plan2 = getUnicodeCharFromSentence(_sentence, 5);
+      if (/[\u{10000}-\u{2ffff}]/u.test(_unicodePlan1Plan2)) {
+        _uni_sentence += _unicodePlan1Plan2 + _sentence.substring(5, _uni_sentences[index].length);
+      } else {
+        let _unicodePlan0 = getUnicodeCharFromSentence(_sentence, 4);
+        if (/[\u{0000}-\u{ffff}]/u.test(_unicodePlan0)) {
+          _uni_sentence += _unicodePlan0 + _sentence.substring(4, _uni_sentences[index].length);
+        } else {
+          _uni_sentence += _sentence;
+        }
+      }
+    }
+  });
+  return _uni_sentence;
+}
+
+function getUnicodeCharFromSentence(sentence, digits) {
+  return String.fromCodePoint(parseInt(sentence.substring(0, digits), 16));
+}
