c5e09ba3e2080ae3c4c3d6f171396a4f9fb04c55
shang-huang <shang.cih.huang@gmail.com>
2019-06-19 10:53:17 +0800

    code refactoring, use three to render directly.

--- components/canvas-text.js
+++ components/canvas-text.js
@@ -1,7 +1,3 @@
-if (typeof AFRAME === "undefined") {
-  throw new Error("Canvas-text need AFRAME to work.");
-}
-
 AFRAME.registerComponent("canvas-text", {
   schema: {
     value: { type: "string" },
@@ -11,116 +7,128 @@ AFRAME.registerComponent("canvas-text", {
   },
 
   init() {
-    // create canvas for text texture, use object3D uuid as id
-    this.canvas = document.createElement("canvas");
-    this.canvas.id = this.el.object3D.uuid;
-    this.canvas.style.display = "none";
-    this.el.appendChild(this.canvas);
-    // create canvas context
-    this.context = this.canvas.getContext("2d");
+    this.canvas = createElement("canvas");
   },
 
-  update: function (oldData) {
-    (oldData != this.data) && this.draw();
-  },
-
-  draw() {
-    let _data = this.data;
-    let _el = this.el;
-    let _canvas = this.canvas;
-    let _context = this.context;
-    // get default font type & apply pixel ratio
-    let _dpi = window.devicePixelRatio;
-    let _fontType = window.getComputedStyle(document.body, null).getPropertyValue("font-family");
-    let _font = `${_data.size * _dpi}px ${_fontType}`;
-    // support unicode character & line breaks 
-    let _sentences = unicodeSentnece(_data.value).replace(/\\n/g, "\n").split("\n");
-    // get text area size & height of each lines
-    let _tempTextAreaEl = document.createElement("div");
-    _tempTextAreaEl.style.display = "inline-block";
-    _sentences.forEach(sentence => {
-      let __tempTextEl = document.createElement("span");
-      __tempTextEl.style.font = _font;
-      __tempTextEl.innerText = sentence;
-      _tempTextAreaEl.appendChild(__tempTextEl);
-      let __tempBreakEl = document.createElement("br");
-      __tempBreakEl.style.lineHeight = 0;
-      _tempTextAreaEl.appendChild(__tempBreakEl);
-    });
-    _el.appendChild(_tempTextAreaEl);
-    let _width = _tempTextAreaEl.offsetWidth;
-    let _height = _tempTextAreaEl.offsetHeight;
-    let _lineHeights = [];
-    _tempTextAreaEl.childNodes.forEach(child => (child.nodeName.toLowerCase() === "span") && _lineHeights.push(child.offsetHeight));
-    _el.removeChild(_tempTextAreaEl);
-    // set canvas size
-    _canvas.width = _width;
-    _canvas.height = _height;
-    // clear previous text
-    _context.clearRect(0, 0, _width, _height);
-    // fill text to canvas
-    _context.font = _font;
-    _context.textBaseline = "middle";
-    _context.fillStyle = _data.color;
-    let _fillHeight = 0;
-    _sentences.forEach((sentence, index) => {
-      let _y = (_lineHeights[index] / 2) + _fillHeight;
-      switch (_data.align) {
-        case "left":
-          _context.textAlign = "left";
-          _context.fillText(sentence, 0, _y);
-          break;
-        case "center":
-          _context.textAlign = "center";
-          _context.fillText(sentence, _width / 2, _y);
-          break;
-        case "right":
-          _context.textAlign = "right";
-          _context.fillText(sentence, _width, _y);
-          break;
-        default:
-          _context.textAlign = "left";
-          _context.fillText(sentence, 0, _y);
-          break;
-      }
-      _fillHeight += _lineHeights[index];
-    });
-    // draw text
-    let _objRatio = 0.0075 / _dpi;
-    _el.hasAttribute("geometry") && _el.removeAttribute("geometry");
-    _el.setAttribute("geometry", { primitive: "plane", width: _width * _objRatio, height: _height * _objRatio });
-    _el.hasAttribute("material") && _el.removeAttribute("material");
-    _el.setAttribute("material", { src: `#${_canvas.id}`, shader: "flat", npot: true, transparent: true });
-  },
-
-  remove() {
-    this.el.innerHTML = "";
+  update(oldData) {
+    (oldData != this.data) && draw(this.el, this.data, this.canvas);
   },
 });
 
-function unicodeSentnece(value) {
-  let _uni_sentences = value.split("\\u");
-  let _uni_sentence = "";
-  _uni_sentences.forEach((_sentence, index) => {
+function draw(el, data, canvas) {
+  // get default font type & apply pixel ratio
+  const dpi = window.devicePixelRatio;
+  const fontType = window.getComputedStyle(document.body, null).getPropertyValue("font-family");
+  const font = `${data.size * dpi}px ${fontType}`;
+  // support unicode character & line breaks 
+  let sentences = unicodeSentnece(data.value).replace(/\\n/g, "\n").split("\n");
+  // create text area
+  let textAreaEl = createElement("div");
+  textAreaEl.style.display = "inline-block";
+  el.appendChild(textAreaEl);
+  // get height of each lines
+  let lineHeights = [];
+  sentences.forEach(sentence => {
+    // append sentence
+    let textEl = createElement("span");
+    textEl.style.font = font;
+    textEl.innerText = sentence;
+    textAreaEl.appendChild(textEl);
+    lineHeights.push(textEl.offsetHeight);
+    // append break for each line
+    let breakEl = createElement("br");
+    breakEl.style.lineHeight = 0;
+    textAreaEl.appendChild(breakEl);
+  });
+  let width = textAreaEl.offsetWidth;
+  let height = textAreaEl.offsetHeight;
+  drawCanvas(canvas, width, height, font, data.color, data.align, sentences, lineHeights);
+  drawEntity(el, canvas, width, height, dpi);
+  el.removeChild(textAreaEl);
+}
+
+function unicodeSentnece(text) {
+  let unicodeSentence = "";
+  let unicodeSentences = text.split("\\u");
+  unicodeSentences.forEach((sentence, index) => {
     if (index === 0) {
-      _uni_sentence += _sentence;
+      unicodeSentence += sentence;
     } else {
-      let _unicodePlan1Plan2 = getUnicodeCharFromSentence(_sentence, 5);
-      if (/[\u{10000}-\u{2ffff}]/u.test(_unicodePlan1Plan2)) {
-        _uni_sentence += _unicodePlan1Plan2 + _sentence.substring(5, _uni_sentences[index].length);
+      let unicodePlan1Plan2 = getUnicodeCharFromSentence(sentence, 5);
+      if (/[\u{10000}-\u{2ffff}]/u.test(unicodePlan1Plan2)) {
+        unicodeSentence += unicodePlan1Plan2 + sentence.substring(5, unicodeSentences[index].length);
       } else {
-        let _unicodePlan0 = getUnicodeCharFromSentence(_sentence, 4);
-        if (/[\u{0000}-\u{ffff}]/u.test(_unicodePlan0)) {
-          _uni_sentence += _unicodePlan0 + _sentence.substring(4, _uni_sentences[index].length);
+        let unicodePlan0 = getUnicodeCharFromSentence(sentence, 4);
+        if (/[\u{0000}-\u{ffff}]/u.test(unicodePlan0)) {
+          unicodeSentence += unicodePlan0 + sentence.substring(4, unicodeSentences[index].length);
         } else {
-          _uni_sentence += _sentence;
+          unicodeSentence += sentence;
         }
       }
     }
   });
-  return _uni_sentence;
+  return unicodeSentence;
 }
 
 function getUnicodeCharFromSentence(sentence, digits) {
   return String.fromCodePoint(parseInt(sentence.substring(0, digits), 16));
+}
+
+function createElement(tag) {
+  return document.createElement(tag);
+}
+
+function drawCanvas(canvas, width, height, font, color, align, sentences, lineHeights) {
+  let context = canvas.getContext("2d");
+  // set canvas size
+  canvas.width = width;
+  canvas.height = height;
+  // clear previous text
+  context.clearRect(0, 0, width, height);
+  // set text style
+  context.font = font;
+  context.textBaseline = "middle";
+  context.fillStyle = color;
+  // draw text line by line based on alignment
+  let incrementHeight = 0;
+  sentences.forEach((sentence, index) => {
+    let y = (lineHeights[index] / 2) + incrementHeight;
+    switch (align) {
+      case "left":
+        context.textAlign = "left";
+        context.fillText(sentence, 0, y);
+        break;
+      case "center":
+        context.textAlign = "center";
+        context.fillText(sentence, width / 2, y);
+        break;
+      case "right":
+        context.textAlign = "right";
+        context.fillText(sentence, width, y);
+        break;
+    }
+    incrementHeight += lineHeights[index];
+  });
+}
+
+function drawEntity(el, canvas, width, height, dpi) {
+  const ratio = 0.0075 / dpi;
+  // claer previous object
+  el.object3D.children.forEach(child => el.object3D.remove(child));
+  // set canvas as NPOT texture
+  // https://github.com/aframevr/aframe/blob/master/src/systems/material.js#L332
+  let texture = new THREE.CanvasTexture(canvas);
+  texture.wrapS = THREE.ClampToEdgeWrapping;
+  texture.wrapT = THREE.ClampToEdgeWrapping;
+  texture.magFilter = THREE.LinearFilter;
+  texture.minFilter = THREE.LinearFilter;
+  // set material to flat shading, transparent & render to double sides
+  let material = new THREE.MeshBasicMaterial({ map: texture });
+  material.precision = "lowp";
+  material.flatShading = true;
+  material.transparent = true;
+  material.side =  THREE.DoubleSide;
+  let geometry = new THREE.PlaneGeometry(width * ratio, height * ratio, 1, 1);
+  let mesh = new THREE.Mesh(geometry, material);
+  el.object3D.add(mesh);
 }
